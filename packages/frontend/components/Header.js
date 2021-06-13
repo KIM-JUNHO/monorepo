69f14b80913c1d01b6ecea2d3125c94ce3fc148b
jh3618-kim <jh3618.kim@samsung.com>
2021-06-14 01:43:46 +0900

    modify Header

--- packages/frontend/components/Header.js
+++ packages/frontend/components/Header.js
@@ -1,11 +1,31 @@
 import styled from '@emotion/styled';
 
 const Header = () => {
-  return <HeaderStyled>Now I AM a real Header</HeaderStyled>;
+  return (
+    <HeaderStyled>
+      <div className="container">
+        <div className="logo">
+          <img src="/images/logo.svg" alt="Sites logo" />
+          <span className="logo-text">Next Movies</span>
+        </div>
+      </div>
+    </HeaderStyled>
+  );
 };
 
 const HeaderStyled = styled.header`
-  background: ${(props) => props.theme.colors.primary};
+  background: '#efefef';
+  padding: 20px;
+  .logo {
+    display: flex;
+    align-items: center;
+    .logo-text {
+      color: #333333;
+      font-weight: bold;
+      font-size: 20px;
+      margin-left: 20px;
+    }
+  }
 `;
 
 export default Header;
