a85b71e6ae89a78786205dd6c1f8d3a25c0d097c
jh3618-kim <jh3618.kim@samsung.com>
2021-06-14 02:39:38 +0900

    nextjs sample fetch

--- packages/frontend/pages/index.js
+++ packages/frontend/pages/index.js
@@ -1,5 +1,28 @@
-const Home = () => {
-  return <div>This is Home Page</div>;
+import fetch from 'isomorphic-unfetch';
+
+const Home = ({
+  movies = [
+    { id: 1, movie_title: 'sdf' },
+    { id: 2, movie_title: 'fd' },
+  ],
+}) => {
+  return (
+    <div className="container">
+      {movies && movies.map((movie) => <div key={movie.id}>{movie.title}</div>)}
+    </div>
+  );
 };
+
+export async function getServerSideProps() {
+  const { API_URL } = process.env;
+  const res = await fetch(`${API_URL}/movies`);
+  const data = await res.json();
+
+  return {
+    props: {
+      movies: data,
+    },
+  };
+}
 
 export default Home;
