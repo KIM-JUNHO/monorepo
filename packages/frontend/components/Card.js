042d04c163872107e0b2388e2da2b3cf022465cb
jh3618-kim <jh3618.kim@samsung.com>
2021-06-14 03:14:34 +0900

    card detail

--- packages/frontend/components/Card.js
+++ packages/frontend/components/Card.js
@@ -1,7 +1,19 @@
 import styled from '@emotion/styled';
 
 const Card = ({ movie }) => {
-  return <CardStyled>{movie.title}</CardStyled>;
+  const { API_URL } = process.env;
+
+  return (
+    <CardStyled>
+      <div className="poster">
+        <img src={API_URL + movie.poster.url} alt="" />
+      </div>
+      <div className="body">
+        <h3>{movie.title}</h3>
+        <p dangerouslySetInnerHTML={{ __html: movie.description }} />
+      </div>
+    </CardStyled>
+  );
 };
 
 const CardStyled = styled.div``;
