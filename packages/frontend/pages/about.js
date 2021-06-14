4d898145434c353e9443d273d7648b76dd1d3bd4
jh3618-kim <jh3618.kim@samsung.com>
2021-06-14 15:01:05 +0900

    use getStaticProps

--- packages/frontend/pages/about.js
+++ packages/frontend/pages/about.js
@@ -1,21 +1,42 @@
 import { NextSeo } from 'next-seo';
+import { Box } from 'reflexbox';
+import fetch from 'isomorphic-unfetch';
+import theme from '../theme/theme';
 
-const About = () => {
+const About = ({ page }) => {
   const SEO = {
-    title: 'About page',
+    title: page.title,
     description: 'Just your normal about page',
 
     openGraph: {
-      title: 'About page',
+      title: page.title,
       description: 'Just your normal about page',
     },
   };
   return (
     <>
       <NextSeo {...SEO} />
-      <h1>I am an about page</h1>
+      <Box theme={theme} variant="container">
+        <Box as="h2" my={40}>
+          {page.title}
+        </Box>
+        <div dangerouslySetInnerHTML={{ __html: page.content }} />
+      </Box>
     </>
   );
 };
+
+export async function getStaticProps() {
+  const { API_URL } = process.env;
+
+  const res = await fetch(`${API_URL}/pages/60c6ee539d55c53fecd3c003`);
+  const data = await res.json();
+
+  return {
+    props: {
+      page: data,
+    },
+  };
+}
 
 export default About;
