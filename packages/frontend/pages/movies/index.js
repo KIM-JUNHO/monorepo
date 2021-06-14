cee5710969fa481a0a43c21da12c5ede0cc1cc00
jh3618-kim <jh3618.kim@samsung.com>
2021-06-14 14:05:17 +0900

    previous next button with page num

--- packages/frontend/pages/movies/index.js
+++ packages/frontend/pages/movies/index.js
@@ -1,7 +1,9 @@
+import { useRouter } from 'next/router';
 import { Flex, Box } from 'reflexbox';
 import theme from '../../theme/theme';
 
-const MoviesPage = ({ movies }) => {
+const MoviesPage = ({ movies, page }) => {
+  const router = useRouter();
   return (
     <Box theme={theme} variant="continaer" pt={40}>
       <ul>
@@ -11,11 +13,15 @@ const MoviesPage = ({ movies }) => {
           </li>
         ))}
       </ul>
+      <Flex mt={40} pl={20} justifyContent="space-between" maxWidth={300}>
+        <button onClick={() => router.push(`/movies?page=${page - 1}`)}>Previous</button>
+        <button onClick={() => router.push(`/movies?page=${page + 1}`)}>Next</button>
+      </Flex>
     </Box>
   );
 };
 
-export async function getServerSideProps() {
+export async function getServerSideProps({ query: { page = 1 } }) {
   const { API_URL } = process.env;
 
   const res = await fetch(`${API_URL}/movies?_limit=3`);
@@ -24,6 +30,7 @@ export async function getServerSideProps() {
   return {
     props: {
       movies: data,
+      page: +page,
     },
   };
 }
