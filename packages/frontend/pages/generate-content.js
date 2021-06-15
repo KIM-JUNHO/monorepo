fba1db33fd102d9d54a61c702b43d1aa40428ac4
jh3618-kim <jh3618.kim@samsung.com>
2021-06-15 14:19:44 +0900

    add create post api

--- packages/frontend/pages/generate-content.js
+++ packages/frontend/pages/generate-content.js
@@ -5,9 +5,26 @@ import theme from '../theme/theme';
 const { publicRuntimeConfig } = getConfig();
 
 function GenerateContent() {
+  const addContent = async () => {
+    const postData = {
+      title: 'some title',
+      content: 'add some content',
+    };
+
+    const generate = await fetch(`${publicRuntimeConfig.API_URL}/posts`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    });
+    const generateResponse = await generate.json();
+    console.log(generateResponse);
+  };
   return (
     <Box theme={theme} variant="container">
-      <button>Generate Strapi Content</button>
+      <button onClick={() => addContent()}>Generate Strapi Content</button>
     </Box>
   );
 }
