98fb836f2e474c7a44b2e96f998b4aa1b0985baf
jh3618-kim <jh3618.kim@samsung.com>
2021-06-15 11:36:45 +0900

    add create movie api

--- packages/frontend/pages/add-movie.js
+++ packages/frontend/pages/add-movie.js
@@ -1,14 +1,61 @@
 import { Box } from 'reflexbox';
 import styled from '@emotion/styled';
 import theme from '../theme/theme';
+import getConfig from 'next/config';
+import { useState } from 'react';
+import { parseCookies } from 'nookies';
+
+const { publicRuntimeConfig } = getConfig();
 
 function AddMovie() {
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieSlug, setMovieSlug] = useState('');
+
+  async function addMovie() {
+    const jwt = parseCookies().jwt;
+
+    const movieInfo = {
+      title: movieTitle,
+      slug: movieSlug,
+    };
+
+    const add = await fetch(`${publicRuntimeConfig.API_URL}/movies`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(movieInfo),
+    });
+
+    const addResponse = await add.json();
+  }
   return (
     <AddMovieStyled>
       <Box theme={theme} variant="container">
         <Box as="h2" my={40}>
           Add movie
         </Box>
+        <form>
+          <input
+            type="text"
+            onChange={(e) => setMovieTitle(e.target.value)}
+            value={movieTitle}
+            placeholder="Movie title"
+          />
+          <br />
+          <input
+            type="text"
+            onChange={(e) => setMovieSlug(e.target.value)}
+            value={movieSlug}
+            placeholder="Movie slug"
+          />
+          <br />
+          <button type="button" onClick={() => addMovie()}>
+            Add Movie
+          </button>
+        </form>
       </Box>
     </AddMovieStyled>
   );
