480c65b37522b25aed8fc55eece8647263e283de
jh3618-kim <jh3618.kim@samsung.com>
2021-06-15 10:40:05 +0900

    call login api

--- packages/frontend/pages/payed-articles.js
+++ packages/frontend/pages/payed-articles.js
@@ -1,20 +1,20 @@
 import { Box } from 'reflexbox';
 import getConfig from 'next/config';
 
-function PayedArticles({ articles }) {
-  console.log(articles);
+function PayedArticles({ articles, authData }) {
+  console.log(articles, authData);
   return (
     <>
       <Box variant="container">
         <Box as="h2" my={40}>
           Payed Articles
         </Box>
-        {articles.map((article) => (
+        {/* {articles.map((article) => (
           <div className="article" key={article.id}>
             <h3>{article.title}</h3>
             <p dangerouslySetInnerHTML={{ __html: article.body }} />
           </div>
-        ))}
+        ))} */}
       </Box>
     </>
   );
@@ -23,12 +23,28 @@ function PayedArticles({ articles }) {
 const { publicRuntimeConfig } = getConfig();
 
 export async function getServerSideProps(ctx) {
+  const loginInfo = {
+    identifier: 'author',
+    password: 'strapi',
+  };
+
+  const login = await fetch(`${publicRuntimeConfig.API_URL}/auth/local`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(loginInfo),
+  });
+  const loginResponse = await login.json();
+
   const res = await fetch(`${publicRuntimeConfig.API_URL}/payed-articles`);
   const articles = await res.json();
 
   return {
     props: {
       articles: articles,
+      authData: loginResponse,
     },
   };
 }
