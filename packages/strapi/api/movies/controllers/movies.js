16f93cb52979282537f883ac45daa6fb37746179
jh3618-kim <jh3618.kim@samsung.com>
2021-06-15 12:18:07 +0900

    add response width, height to find query

--- packages/strapi/api/movies/controllers/movies.js
+++ packages/strapi/api/movies/controllers/movies.js
@@ -1,8 +1,34 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require('strapi-utils');
+// const sizeOf = require('image-size');
+// const path = require('path');
 
-module.exports = {};
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.movies.search(ctx.query);
+    } else {
+      entities = await strapi.services.movies.find(ctx.query);
+    }
+
+    return entities.map((entity) => {
+      const movie = sanitizeEntity(entity, { model: strapi.models.movies });
+      //   if (movie.poster !== null) {
+      // const dimensions = sizeOf(path.resolve(__dirname, '../../../public' + movie.poster.url));
+      //   movie.poster.width = dimensions.width;
+      //   movie.poster.height = dimensions.height;
+      // movie.poster.width = 200;
+      // movie.poster.height = 200;
+      //   }
+      return movie;
+    });
+  },
+};
