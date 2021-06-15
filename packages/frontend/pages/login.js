c75449b451a36a872559381fb249b612f69800dd
jh3618-kim <jh3618.kim@samsung.com>
2021-06-15 10:55:46 +0900

    add login api

--- packages/frontend/pages/login.js
+++ packages/frontend/pages/login.js
@@ -2,10 +2,34 @@ import { Box } from 'reflexbox';
 import getConfig from 'next/config';
 import styled from '@emotion/styled';
 import theme from '../theme/theme';
+import { useState } from 'react';
 
 const { publicRuntimeConfig } = getConfig();
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleLogin() {
+    const loginInfo = {
+      identifier: username,
+      password: password,
+    };
+
+    const login = await fetch(`${publicRuntimeConfig.API_URL}/auth/local`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginInfo),
+    });
+
+    const loginResponse = await login.json();
+
+    console.log(loginResponse);
+  }
+
   return (
     <>
       <LoginStyled>
@@ -13,6 +37,15 @@ function Login() {
           <Box as="h2" my={40}>
             You need to login to access this page
           </Box>
+          <form>
+            <input type="email" onChange={(e) => setUsername(e.target.value)} value={username} />
+            <br />
+            <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+            <br />
+            <button type="button" onClick={() => handleLogin()}>
+              Login
+            </button>
+          </form>
         </Box>
       </LoginStyled>
     </>
