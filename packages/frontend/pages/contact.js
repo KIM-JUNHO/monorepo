52a9d808433b7db207fba486ac1206734b6052ba
jh3618-kim <jh3618.kim@samsung.com>
2021-06-15 10:17:39 +0900

    use i18n namespace

--- packages/frontend/pages/contact.js
+++ packages/frontend/pages/contact.js
@@ -1,5 +1,28 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
+import { Box } from 'reflexbox';
+
 const Contact = () => {
-  return <div>I am an Contact page</div>;
+  const { t } = useTranslation('footer');
+  return (
+    <>
+      <Box variant="container">
+        <Box as="h2" my={40}>
+          {t('common:Latest Movies')}
+          <br />
+          {t('contact:Contact')}
+        </Box>
+        <div>{t('contact:This is a contact page')}</div>
+      </Box>
+    </>
+  );
 };
+export async function getServerSideProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'contact'])),
+    },
+  };
+}
 
 export default Contact;
